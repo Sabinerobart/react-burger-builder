5f16edf9ce15e1c6fe146c92bd79926db9438f19
Sabine Robart <sabine.robart@protonmail.com>
2019-10-19 08:22:01 +0200

    [Add] the basic layout of the BurgerBuilder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+
 import "./App.css";
+
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends React.Component {
   render() {
     return (
       <Layout>
-        <p>...</p>
+        <BurgerBuilder />
       </Layout>
     );
   }

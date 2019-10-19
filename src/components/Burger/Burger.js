2e5875e0f19b144da068a1b52609312a824762d6
Sabine Robart <sabine.robart@protonmail.com>
2019-10-19 15:23:41 +0200

    [Add] .reduce() to count the nb of ingredients and add a message if there are none

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
   // transform a key/value object into an array of the keys * their value
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    }); // spread to get a new array
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      }); // spread to get a new array
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Go ahead, start adding ingredients ! :)</p>;
+  }
+
+  console.log(transformedIngredients);
 
   return (
     <div className="Burger">

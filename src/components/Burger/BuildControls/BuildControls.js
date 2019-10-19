a8e3d724d4f5fe2ae321a9769ee18cb692e25831
Sabine Robart <sabine.robart@protonmail.com>
2019-10-19 15:47:00 +0200

    [Add] the 'add ingredients' button and style them

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,21 @@
 import React from "react";
+
+import "./BuildControls.css";
 import BuildControl from "./BuildControl/BuildControl";
 
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
 const BuildControls = props => (
-  <React.Fragment>
-    <BuildControl />
-  </React.Fragment>
+  <section className="BuildControls">
+    {controls.map((control, key) => {
+      return <BuildControl key={key} label={control.label} />;
+    })}
+  </section>
 );
 
 export default BuildControls;

c56957579f7d31eabb95e66b090449b903a31e45
Sabine Robart <sabine.robart@protonmail.com>
2019-10-19 15:35:45 +0200

    [Add] the BuildControl buttons and style them

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BuildControl from "./BuildControl/BuildControl";
+
+const BuildControls = props => (
+  <React.Fragment>
+    <BuildControl />
+  </React.Fragment>
+);
+
+export default BuildControls;

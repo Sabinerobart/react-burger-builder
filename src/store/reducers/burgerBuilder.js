30166f1d28982d2d2177fc8634e7795d62b86000
Sabine Robart <sabine.robart@protonmail.com>
2020-04-22 12:04:57 +0200

    Reset the base price after ordering

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -44,6 +44,7 @@ const reducer = (state = initialState, action) => {
           cheese: action.ingredients.cheese,
           meat: action.ingredients.meat,
         },
+        totalPrice: 4,
         error: false // in case we had an error earlier, that we don't have now
       };
 
